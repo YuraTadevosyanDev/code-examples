3085d2ff28b6f4da4935b78936749f871faebf8b
Yura <yuratadevosyan.dev@gmail.com>
2022-11-15 09:42:04 +0400

    Adding layout and Home page

--- gyumri-brand-shop/src/App.js
+++ gyumri-brand-shop/src/App.js
@@ -1,9 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 
+// Components
+import { Layouts } from './components';
+
+// Pages
+import {
+    Home,
+} from './pages';
+
 function App() {
     return (
         <div className="App">
             <Routes>
+                <Route element={<Layouts />}>
+                    <Route path="/" element={<Home />} />
+                </Route>
             </Routes>
         </div>
     );
